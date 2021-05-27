320fb5a8d3c985a96ab553e4548155e187395e70
mbalandari <m.balandari.toroghi@gmail.com>
2021-05-27 16:49:02 +0300

    map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 
-function getName(name) {
-  return name;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 
 function App() {
@@ -9,10 +24,23 @@ function App() {
   return (
 
     <div>
-      <h1>Sup {getName("Maz")}?</h1>
+      <h1>Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      {list.map(function (item) {
+        return (
+          <div key={item.objectID}>
+            <span><a href={item.url}>{item.title} </a></span>
+            <span>{item.author} </span>
+            <span>{item.num_comments} </span>
+            <span>{item.points}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
